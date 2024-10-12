cde0fbb9699d2ce84260f0b6c14df401e915c15c
Artem <artemmazurik2005@mail.ru>
2024-10-12 23:01:47 +0300

    new html templates, new code

--- app.py
+++ app.py
@@ -5,9 +5,26 @@ from flask import render_template
 
 app = flask.Flask(__name__)
 
+class Contact:
+    def __init__(self, id, name, phone, email):
+        self.id = id
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+
+contacts = [
+    Contact(1, "Alice", "123456789", "alice@example.com"),
+    Contact(2, "Bob", "987654321", "bob@example.com")
+]
+
 @app.route('/')
 def contact_list():
     return render_template('base.html')
+
+@app.route('/all_contacts')
+def all_contacts():
+    return render_template('contacts.html', contacts=contacts)
 
 
 if __name__ == '__main__':
