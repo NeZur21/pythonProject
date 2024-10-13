ad22b625db3acf8642ff3398ec5975e1a00487c5
Artem <artemmazurik2005@mail.ru>
2024-10-13 17:37:13 +0300

    added edit page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import flask
 import db
 
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = flask.Flask(__name__)
 
@@ -18,12 +18,23 @@ for i in db.rows:
     contacts.append(Contact(i.id, i.Name, i.Phone, i.Email))
 
 @app.route('/')
-def contact_list():
+def main():
     return render_template('base.html')
 
 @app.route('/all_contacts')
 def all_contacts():
     return render_template('contacts.html', contacts=contacts)
+
+@app.route('/edit_contact/<int:contact_id>', methods=['GET', 'POST'])
+def edit_contact(contact_id):
+    contact = next((c for c in contacts if c.id == contact_id), None)
+    if request.method == 'POST':
+        contact.name = request.form['name']
+        contact.phone = request.form['phone']
+        contact.email = request.form['email']
+        return redirect(flask.url_for('all_contacts'))
+    return render_template('edit_contact.html', contact=contact)
+
 
 
 if __name__ == '__main__':
